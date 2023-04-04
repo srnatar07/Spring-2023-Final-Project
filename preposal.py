4c6ee6bba5e90ede11735ec9c7317ad91e6e60af
srnatar07 <129218136+srnatar07@users.noreply.github.com>
2023-04-04 19:51:07 -0400

    Add files via upload

---
+++ preposal.py
@@ -0,0 +1,17 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+A program which acts as a travel agent and takes in the user's input about where they want to travel, helps to plan an itinerary, and provides translations for
+common words and phrases at their chosen travel destination.
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+I don't plan to collaborate with anyone 
+
+
+## Do you have any questions of your own?
+
+No, I don't have any questions as of now.
+
+
