a11f7826b9927d6545d348b0fa01d1b11b24f2df
srnatar07 <129218136+srnatar07@users.noreply.github.com>
2023-04-06 22:59:56 -0400

    Add files via upload

---
+++ finalproposal.py
@@ -0,0 +1,54 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+Travel Agent 
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+Software that takes on the roll of a travel agent and helps the user choose a location of travel as well as helps the user to plan an itinerary for
+the trip. The program will also help the user with budgeting for their trip and provide common translations and phrases in the language of their chosen destination. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+The software will ask the user for a budget for their trip. Using Ren'Py, it will then show the user a picture of the world map and indicate possible
+travel destinations which the user can choose from. The user can look through the destinations and the software will provide some information and facts about each destination
+using if statements. When the user chooses a location, the software will help the user to budget their trip and plan an itinerary. A dictionary will be created with the
+relative pricing of each potential activity and when choosing an activity for the itinerary this price will be subtracted from the budget. Once the trip is planned, the
+software will provide the user with some words and common phrases in the language of the country of destination. The program will then end by saying goodbye to the
+user in the language of the destination country.   
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+Not applicable 
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+Not applicable
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+No matter what I would like to be able to visualize the map and get the user to choose a location. I would also like the user to be able to plan an itinerary
+for their trip, and say goodbye in the language of their country of destination.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+A better outcome would be to, in addition to the good outcome, also provide common words and phrases in the language of the country of destination. 
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+The best outcome would be to be able to complete everything detailed above. In addition to the better outcome, for the best outcome I would like to also be able to help the user to
+budget their trip. The enture project would then consist of visualizing the map and indicating possible locations, allowing the user to look through information about each location,
+allowing the user to choose a location, asking the user for a budget, helping the user come up with an itinerary, keeping the user within the budget, providing the user with words
+and phrases from the destination country, and saying goodbye in that language when the program ends.  
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+The next steps are to research different countries, their languages, and tourist atttractions. After that I would need to aquire knowledge
+about Ren'Py so that I can use it for visualization. I will then work on visualizing the world map. The next step would be to write the software to get
+the user's input regarding their budget through an input statement and assigning this input to a variable. Then I would add facts and information about each country
+which the user would be able to look through when going through each country. For when the user has chosen where they would like to travel to, I will then work
+on the part of the program which helps the user put together an itineray as well as helps the user to stay within their budget. The software would inform the user
+about the cost of each item on the itinerary and subtract this from the inputed budget. The next step would be to create a dictionary with the country and the way to
+say goodbye in each country so that when the program ends, the program will print out goodbye in the language of the destination country. 
